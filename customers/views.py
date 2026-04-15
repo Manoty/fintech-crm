3dbc2062e06daa7aab04bbaa3334cecfff249604
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-15 22:04:09 +0300

    add customer views

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('-created_at')
+    serializer_class = CustomerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['full_name', 'phone_number', 'email']
