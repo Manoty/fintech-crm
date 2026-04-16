1f9eaa659d241c50288864d3fddb3b54af38205a
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-16 20:20:11 +0300

    Update REST_FRAMEWORK settings

--- core/settings.py
+++ core/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
 # ─── Django REST Framework ────────────────────────────────────────────────────
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
